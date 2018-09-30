8c7caab61776505ea34def8fd86a3b1c71726e90
scmilee <drudged@oregonstate.edu>
2018-09-29 17:15:00 -0700

    add new functions like chekcanswer and update question to help reduce code duplication

--- app/src/main/java/edu/drudgedoregonstate/drudgedylangeoquiz/MainActivity.java
+++ app/src/main/java/edu/drudgedoregonstate/drudgedylangeoquiz/MainActivity.java
@@ -5,35 +5,68 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.Button;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
 
+    private int mCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTrueButton = findViewById(R.id.true_button);
-        mFalseButton = findViewById(R.id.false_button);
 
+        mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
-                toast.show();
+                checkAnswer(true);
             }
         });
+        mFalseButton = findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
-                toast.show();
+                checkAnswer(false);
             }
         });
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            } });
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        updateQuestion();
+
+    }
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressed) {
+        boolean answerIs = mQuestionBank[mCurrentIndex].ismTrueOrFalse();
+        int messageResId = 0;
+        if (answerIs == userPressed ) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
     }
 }
--- app/src/main/java/edu/drudgedoregonstate/drudgedylangeoquiz/Question.java
+++ app/src/main/java/edu/drudgedoregonstate/drudgedylangeoquiz/Question.java
@@ -10,7 +10,7 @@ public class Question {
     }
 
 
-    public int getmQuestionId() {
+    public int getQuestion() {
         return mQuestionId;
     }
 
